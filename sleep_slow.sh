fad3a911803d482db2c3f0c487655e77489b4313
Zain ul abidin <zainengineer@gmail.com>
2018-01-09 21:36:19 +1030

    sleep_slow: run as sudo if not already sudo

--- sleep_slow.sh
+++ sleep_slow.sh
@@ -5,8 +5,13 @@
 #observe that few times that when it goes to lock screen mode
 #and after many minutes I take out power, it goes to sleep (inactivity+no power)
 #then it wakes up properly otherwise it does not wake up
-sudo echo "sleeping in 20 seconds, lock your screen"
-sudo sleep 5s
+if [[ $EUID -ne 0 ]]; then
+  echo "not root so running in sudo"
+  sudo sleep_slow 
+  exit 1
+fi
+sudo echo "will lock your screen and put computer to sleep"
+sudo sleep 2s
 gnome-screensaver-command -l
 sudo sleep 8s
 sudo systemctl suspend
